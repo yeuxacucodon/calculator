16a9ca76de1db904c111d40576da66162955ccb1
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-04-28 22:10:30 +0700

    First commit

---
+++ calculator.js
@@ -0,0 +1,13 @@
+const display = document.getElementById("screen");
+
+const wipe = () => {
+	display.value = "";
+};
+
+const show = (n) => {
+	display.value += n;
+};
+
+const calc = () => {
+	display.value = eval(display.value);
+};
